d5ff18fa8b3d011e887fc2c2c36add97d94a22e7
Caracalatcoding <113795210+Caracalatcoding@users.noreply.github.com>
2023-03-02 12:48:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+// writing on console
+
+console.log("My first console project");
+
+// variables
+
+var first_name = "Ranbir";
+var last_name = "Kapoor";
+
+var a = 10;
+var b = 5;
+
+//joining two letters
+
+full_name = first_name.concat(last_name);
+console.log(full_name);
+
+// adding two numbers
+
+var add = a + b;
+console.log(add);
+
+//subtracting two numbers
+
+var dif = a - b;
+console.log(dif)
+
+//multiplying two numbers
+
+var product = a * b;
+console.log(product);
+
+//dividing two numbers
+
+var product2 = a / b;
+console.log(product2);
+
